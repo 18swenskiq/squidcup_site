6711b56866b2ff98ed4ef55ff44e7f13de3ce070
Quinton Swenski <18swenskiq@gmail.com>
2025-03-11 16:55:36 -0400

    Add cdk

--- frontend/src/app/main-view/main-view.component.ts
+++ frontend/src/app/main-view/main-view.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
+import { Router, RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-main-view',
   standalone: true,
-  imports: [],
+  imports: [RouterModule],
   templateUrl: './main-view.component.html',
   styleUrl: './main-view.component.scss'
 })
 export class MainViewComponent {
-
+  constructor(private router: Router) {}
 }
