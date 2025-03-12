3a5d912aca18cb7c18774dc32b9dc6e2f18487c9
Quinton Swenski <18swenskiq@gmail.com>
2025-03-12 10:33:37 -0400

    Attempt to add site build

--- cdk/lib/frontend-stack.ts
+++ cdk/lib/frontend-stack.ts
@@ -1,16 +1,27 @@
 import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
 import * as s3 from 'aws-cdk-lib/aws-s3';
+import * as s3deploy from 'aws-cdk-lib/aws-s3-deployment';
+import * as path from 'path';
 
 export class FrontendStack extends Construct {
   constructor(scope: Construct, id: string) {
     super(scope, id);
 
-    new s3.Bucket(this, "squidcup_site_frontend", {
+    // Create the S3 bucket
+    const websiteBucket = new s3.Bucket(this, "squidcup_site_frontend", {
       encryption: s3.BucketEncryption.S3_MANAGED, 
       removalPolicy: cdk.RemovalPolicy.DESTROY,
       publicReadAccess: true,
       blockPublicAccess: s3.BlockPublicAccess.BLOCK_ACLS,
+      websiteIndexDocument: 'index.html',
+      websiteErrorDocument: 'index.html',
+    });
+
+    // Deploy the frontend assets to the S3 bucket
+    new s3deploy.BucketDeployment(this, 'DeployWebsite', {
+      sources: [s3deploy.Source.asset(path.join(__dirname, '../../assets'))],
+      destinationBucket: websiteBucket,
     });
   }
 }
