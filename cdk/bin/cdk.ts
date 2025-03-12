27342021b5b674ea5ad339629beb8eee2a3a9d7b
Quinton Swenski <18swenskiq@gmail.com>
2025-03-12 11:52:48 -0400

    Update stacks

--- cdk/bin/cdk.ts
+++ cdk/bin/cdk.ts
@@ -1,17 +1,21 @@
 #!/usr/bin/env node
-import 'source-map-support/register';
 import * as cdk from 'aws-cdk-lib';
 import { ApiStack } from '../lib/api-stack';
 import { FrontendStack } from '../lib/frontend-stack';
 
 const app = new cdk.App();
 
-// Create API stack
-new ApiStack(app, 'ApiStack', {
-  stackName: 'SquidCupSite-ApiStack',
-});
+const stackId = app.node.tryGetContext('stackId');
 
-// Create Frontend stack that will be deployed after the API
-new FrontendStack(app, 'FrontendStack', {
-  stackName: 'SquidCupSite-FrontendStack',
-});
+// If stackId is specified, only instantiate the requested stack
+if (stackId === 'ApiStack') {
+  new ApiStack(app, 'SquidCupSite-ApiStack');
+} else if (stackId === 'FrontendStack') {
+  new FrontendStack(app, 'SquidCupSite-FrontendStack');
+} else {
+  // If no specific stack is requested, instantiate both (default behavior)
+  new ApiStack(app, 'SquidCupSite-ApiStack');
+  new FrontendStack(app, 'SquidCupSite-FrontendStack');
+}
+
+app.synth();
