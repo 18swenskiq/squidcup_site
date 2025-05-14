6c948500e4fd7451dd31f4ab8172705e8a16cfbf
Quinton Swenski <18swenskiq@gmail.com>
2025-05-14 19:49:37 -0400

    Update frontend stack to use cloudfront for delivery

--- cdk/lib/frontend-stack.ts
+++ cdk/lib/frontend-stack.ts
@@ -2,6 +2,8 @@ import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
 import * as s3 from 'aws-cdk-lib/aws-s3';
 import * as s3deploy from 'aws-cdk-lib/aws-s3-deployment';
+import * as cloudfront from 'aws-cdk-lib/aws-cloudfront';
+import * as origins from 'aws-cdk-lib/aws-cloudfront-origins';
 import * as path from 'path';
 import * as fs from 'fs';
 
@@ -11,31 +13,45 @@ export class FrontendStack extends cdk.Stack {
 
     // Create the S3 bucket
     const websiteBucket = new s3.Bucket(this, "squidcup_site_frontend", {
-      encryption: s3.BucketEncryption.S3_MANAGED, 
+      encryption: s3.BucketEncryption.S3_MANAGED,
       removalPolicy: cdk.RemovalPolicy.DESTROY,
-      publicReadAccess: true,
-      blockPublicAccess: new s3.BlockPublicAccess({
-        blockPublicAcls: false,
-        blockPublicPolicy: false,
-        ignorePublicAcls: false,
-        restrictPublicBuckets: false
-      }),
-      websiteIndexDocument: 'index.html',
-      websiteErrorDocument: 'index.html',
+      blockPublicAccess: s3.BlockPublicAccess.BLOCK_ALL,
+    });
+
+    // Create CloudFront distribution
+    const distribution = new cloudfront.Distribution(this, 'SquidcupDistribution', {
+      defaultBehavior: {
+        origin: new origins.S3StaticWebsiteOrigin(websiteBucket),
+        viewerProtocolPolicy: cloudfront.ViewerProtocolPolicy.REDIRECT_TO_HTTPS,
+      },
+      defaultRootObject: 'index.html',
+      errorResponses: [
+        {
+          httpStatus: 404,
+          responseHttpStatus: 200,
+          responsePagePath: '/index.html'
+        }
+      ]
     });
 
     // Check if assets directory exists before attempting deployment
     const assetsPath = path.join(__dirname, '../assets/browser');
     if (fs.existsSync(assetsPath)) {
-      // Deploy the frontend assets to the S3 bucket only if assets exist
       new s3deploy.BucketDeployment(this, 'DeployWebsite', {
         sources: [s3deploy.Source.asset(assetsPath)],
         destinationBucket: websiteBucket,
+        distribution,
+        distributionPaths: ['/*']
       });
     } else {
-      // Log that assets don't exist - this won't fail the CDK synthesis
       console.log('Frontend assets directory not found at:', assetsPath);
       console.log('Skipping frontend deployment - bucket created without content');
     }
+
+    // Output the CloudFront URL
+    new cdk.CfnOutput(this, 'DistributionDomainName', {
+      value: distribution.distributionDomainName,
+      description: 'Website URL'
+    });
   }
 }
